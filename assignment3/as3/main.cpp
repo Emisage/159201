d299ac49c0545cac21c8964f8a9861d3c6372932
mooophy <MoophyAlan@gmail.com>
2014-12-06 12:17:53 +1300

    finished first version for queue, not tested yet 	modified:   assignment3/as3/main.cpp

--- assignment3/as3/main.cpp
+++ assignment3/as3/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stdexcept>
 
 namespace ads {
 
@@ -8,24 +9,25 @@ class Queue
     struct Node
     {
         T data_;
-        Node* nest_;
+        Node* next_;
     };
+
 public:
     using SizeType  =   std::size_t;
 
-    void join()
+    void join(T t)
     {
-
+        do_join(std::move(t));
     }
 
     void leave()
     {
-
+        do_leave();
     }
 
-    T const& front()
+    T const& front() const
     {
-
+        return head_->data_;
     }
 
 
@@ -36,13 +38,41 @@ public:
 
     bool empty()const
     {
-
+        return head_ or tail_;
     }
 
 private:
     Node* head_{nullptr};
     Node* tail_{nullptr};
     SizeType size_{0};
+
+    void do_join(T && t) noexcept
+    {
+        if(empty())
+            head_ = tail_        = new Node{std::move(t), nullptr};
+        else
+            tail_ = tail_->next_ = new Node{std::move(t), nullptr};
+        ++size_;
+    }
+
+    void do_leave()
+    {
+        if(size_ == 0)
+            throw std::runtime_error{"underflow!"};
+
+        if(size_ == 1)
+        {
+            delete head_;
+            head_ = tail_ = nullptr;
+        }
+        else
+        {
+            auto tmp = head_;
+            head_ = head_->next_;
+            delete tmp;
+        }
+        --size_;
+    }
 };
 
 }//namespace
