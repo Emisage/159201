5b20e24712e8805969eecbdcc7c3a530ed3bac66
mooophy <MoophyAlan@gmail.com>
2014-12-06 11:39:48 +1300

    add skeleton 	modified:   assignment3/as3/main.cpp

--- assignment3/as3/main.cpp
+++ assignment3/as3/main.cpp
@@ -1,6 +1,51 @@
 #include <iostream>
 
+namespace ads {
 
+template<typename T>
+class Queue
+{
+    struct Node
+    {
+        T data_;
+        Node* nest_;
+    };
+public:
+    using SizeType  =   std::size_t;
+
+    void join()
+    {
+
+    }
+
+    void leave()
+    {
+
+    }
+
+    T const& front()
+    {
+
+    }
+
+
+    SizeType size()const
+    {
+        return size_;
+    }
+
+    bool empty()const
+    {
+
+    }
+
+private:
+    Node* head_{nullptr};
+    Node* tail_{nullptr};
+    SizeType size_{0};
+};
+
+}//namespace
 
 int main()
 {
