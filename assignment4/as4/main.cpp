a5af30d996268f0ba11d20ac4387687f9aec34d9
mooophy <MoophyAlan@gmail.com>
2014-12-12 15:18:35 +1300

    add operator + 	modified:   assignment4/as4/main.cpp

--- assignment4/as4/main.cpp
+++ assignment4/as4/main.cpp
@@ -52,12 +52,21 @@ private:
 };
 
 
+template<typename T>
+struct BigNumber;
+
+template<typename T>
+BigNumber<T> operator+(BigNumber<T> const& lhs, BigNumber<T> const& rhs);
+
+
 /**
  * @brief Section B : The BigNumber struct
  */
 template<typename T>
-struct BigNumber : public List<T>
+struct BigNumber : private List<T>
 {
+    friend BigNumber operator+ <T>(BigNumber const&, BigNumber const&);
+
     using Super = List<T>;
     using Super::push_front;
 
@@ -74,6 +83,33 @@ struct BigNumber : public List<T>
 };
 
 
+template<typename T>
+BigNumber<T> operator+(BigNumber<T> const& lhs, BigNumber<T> const& rhs)
+{
+    BigNumber<T> sum;
+    int carry = 0;
+    auto l=lhs->head_, r=rhs->tail_;
+    for(; l and r; l=l->next_, r=r->next_)              //add two numbers
+    {
+        auto digit_sum = carry + l->value_ + r->value_ ;
+        sum.push_front(digit_sum%10);
+        carry = digit_sum/10;
+    }
+
+    for(auto rest = (l ? l : r); rest; rest=rest->next_)//when either one exhausted
+    {
+        auto digit_sum = carry + rest->value_;
+        sum.push_front(digit_sum%10);
+        carry = digit_sum/10;
+    }
+
+    if(carry)                                           //for the last carry
+        sum.push_front(carry);
+
+    return sum;
+}
+
+
 }//namespace
 
 
@@ -81,6 +117,8 @@ int main()
 {
     ads::BigNumber<int> num{"1264"};
     num.print();
+
+
     return 0;
 }
 
